09a53671f214ef4683d4d62b74d7ad802d1ef29c
17u5h <102058870+17u5h@users.noreply.github.com>
2023-05-05 19:30:30 +0700

    cookie attention and confirmation styles and adaptive styles minor changes

--- src/components/CookieAttention/CookieConfirmation.tsx
+++ src/components/CookieAttention/CookieConfirmation.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './styles.module.css'
 import CookieAcceptButton from "./CookieAcceptButton";
+import UiCloseButton from "../UI/UICloseButton";
 
 
 type Props = {
@@ -12,6 +13,7 @@ const CookieConfirmation = ({onClick, onMouseLeave}: Props) => {
 	return (
 		<div className={styles.cookieConfirmation} onClick={onClick} onMouseLeave={onMouseLeave}>
 			<CookieAcceptButton/>
+			<UiCloseButton onClick={onClick}/>
 		</div>
 	);
 };
